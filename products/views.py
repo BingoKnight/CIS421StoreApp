5cca166eb6ab713ec4294a19a227b2cfd9767d93
Nathan Hessling <nrhessling@gmail.com>
2020-03-24 02:24:59 -0400

    added put and delete functions

--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from django.db import connection
 from .serializer import ProductSerializer
-from .models import Product
 
 
 def product_select_query_builder(validated_dict):
@@ -19,10 +18,16 @@ def product_select_query_builder(validated_dict):
     first_itr = True
     for key, value in validated_dict.items():
         if first_itr and key != 'csrfmiddlewaretoken':
-            query += key.lower() + '="' + value.lower() + '"'
+            if isinstance(value, int):
+                query += key + '="' + str(value) + '"'
+            else:
+                query += key + '="' + value.lower().strip() + '"'
             first_itr = False
         elif key != 'csrfmiddlewaretoken':
-            query += ' AND ' + key.lower() + '="' + value.lower() + '"'
+            if isinstance(value, int):
+                query += ' AND ' + key + '="' + str(value) + '"'
+            else:
+                query += ' AND ' + key + '="' + value.lower().strip() + '"'
 
     query += ';'
     print(query)
@@ -45,10 +50,11 @@ def convert_to_dict(products_list):
     return new_list
 
 
-def get_validated_list(product_dict, is_post):
-    valid_keys_list = ['name', 'description', 'category', 'price', 'quantity', 'csrfmiddlewaretoken']
+def get_validated_dict(product_dict, is_post):
+    valid_keys_list = ['name', 'description', 'category', 'price', 'quantity']
 
-    del product_dict['csrfmiddlewaretoken']
+    if 'csrfmiddlewaretoken' in product_dict:
+        del product_dict['csrfmiddlewaretoken']
 
     keys_to_delete = [key for key, value in product_dict.items() if value is None or value is '']
     for key in keys_to_delete:
@@ -57,9 +63,9 @@ def get_validated_list(product_dict, is_post):
     if not product_dict:
         raise ValueError('empty object')
 
-    for key in product_dict.keys():
-        if key not in valid_keys_list:
-            raise ValueError('invalid product object key')
+    # for key in product_dict.keys():
+    #     if key not in valid_keys_list:
+    #         raise ValueError('invalid product object key')
 
     if is_post:
         for key in valid_keys_list:
@@ -80,10 +86,10 @@ class ProductAPIView(generics.GenericAPIView):
         first_itr = True
         for key in validated_dict:
             if first_itr and key != 'csrfmiddlewaretoken':
-                query += key.lower().strip()
+                query += key
                 first_itr = False
             elif key != 'csrfmiddlewaretoken':
-                query += ', ' + key.lower().strip()
+                query += ', ' + key
 
         query += ') VALUES ('
         first_itr = True
@@ -97,9 +103,24 @@ class ProductAPIView(generics.GenericAPIView):
         query += ');'
         return query
 
+    @staticmethod
+    def product_update_query_builder(validated_dict):
+        query = 'UPDATE products_product SET '
+
+        first_itr = True
+        for key, value in validated_dict.items():
+            if first_itr and key != 'csrfmiddlewaretoken' and key != 'id':
+                query += key + '="' + value.lower().strip() + '"'
+                first_itr = False
+            elif key != 'csrfmiddlewaretoken' and key != 'id':
+                query += ', ' + key + '="' + value.lower().strip() + '"'
+
+        query += ' WHERE id=' + str(validated_dict['id']) + ';'
+        return query
+
     def post(self, request, *args, **kwargs):
         try:
-            validated_dict = get_validated_list(request.data.copy(), True)
+            validated_dict = get_validated_dict(request.data.copy(), True)
         except ValueError as error:
             return Response({'details': format(error)}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -117,7 +138,25 @@ class ProductAPIView(generics.GenericAPIView):
         return Response(products_list)
 
     def put(self, request):
-        pass
+        cursor = connection.cursor()
+        try:
+            validated_dict = get_validated_dict(request.data.copy(), False)
+        except ValueError as error:
+            return Response({'details': format(error)}, status=status.HTTP_400_BAD_REQUEST)
+
+        query = self.product_update_query_builder(validated_dict)
+        cursor.execute(query)
+
+        updated_product = product_select_query_builder(validated_dict)
+        cursor.execute(updated_product)
+        result = cursor.fetchall()
+
+        return Response(convert_to_dict(result))
+
+    def delete(self, request):
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM products_product WHERE id=' + str(request.data['id']) + ';')
+        return Response({'details': 'product succesfully deleted'})
 
     def get_queryset(self):
         pass
@@ -130,7 +169,7 @@ class ProductSearchAPIView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         search_query = request.data.copy()
         try:
-            search_query = get_validated_list(search_query, False)
+            search_query = get_validated_dict(search_query, False)
         except ValueError as error:
             return Response({'details': format(error)}, status=status.HTTP_400_BAD_REQUEST)
 
