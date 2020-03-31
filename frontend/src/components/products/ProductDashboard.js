b92d532ef113d8f17de70fa0180e767dc5fe5c1a
jdudley4 <51183959+jdudley4@users.noreply.github.com>
2020-03-30 21:10:43 -0400

    frontend
    frontend added

---
+++ frontend/src/components/products/ProductDashboard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Form from './Form';
+import Leads from './Products';
+
+export default function ProductDashboard() {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
+
